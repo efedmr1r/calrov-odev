8851d13aa5425e63b295614ec2c7aea30f2d4451
Yiğit <yigitozdemir1299@gmail.com>
2022-10-24 18:12:09 +0300

    publish raw images

--- catkin_ws/build/catkin_generated/setup_cached.sh
+++ catkin_ws/build/catkin_generated/setup_cached.sh
@@ -5,12 +5,9 @@
 # it emulates the modifications of the setup script without recurring computations
 
 # new environment variables
-export PYTHONPATH='/home/thunderstorm010/mambaforge/envs/ros_env/lib/python3.9/site-packages'
 
 # modified environment variables
 export CMAKE_PREFIX_PATH="/home/thunderstorm010/calrov-odev/catkin_ws/devel:$CMAKE_PREFIX_PATH"
-export PATH='/home/thunderstorm010/mambaforge/envs/ros_env/bin:/home/thunderstorm010/mambaforge/envs/ros_env/x86_64-conda-linux-gnu/sysroot/usr/bin:/home/thunderstorm010/mambaforge/condabin:/home/thunderstorm010/.cargo/bin:/home/thunderstorm010/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin:/snap/bin:/home/thunderstorm010/.local/share/JetBrains/Toolbox/scripts'
-export PKG_CONFIG_PATH="/home/thunderstorm010/calrov-odev/catkin_ws/devel/lib/pkgconfig:$PKG_CONFIG_PATH"
 export PWD='/home/thunderstorm010/calrov-odev/catkin_ws/build'
 export ROSLISP_PACKAGE_DIRECTORIES='/home/thunderstorm010/calrov-odev/catkin_ws/devel/share/common-lisp'
 export ROS_PACKAGE_PATH="/home/thunderstorm010/calrov-odev/catkin_ws/src:$ROS_PACKAGE_PATH"
--- catkin_ws/src/calrov_odev_impl/scripts/img_publisher.py
+++ catkin_ws/src/calrov_odev_impl/scripts/img_publisher.py
@@ -2,28 +2,29 @@ import rospy
 from cv_bridge import CvBridge
 import cv2
 import os
+from sensor_msgs.msg import Image
 from std_msgs.msg import String
 
 def talker():
-    pub = rospy.Publisher('chatter', String, queue_size=10)
+    pub = rospy.Publisher('chatter', Image, queue_size=10)
     rospy.init_node('talker', anonymous=True)
     rate = rospy.Rate(10) # 10hz
 
     bridge = CvBridge()
 
     imgs = []
-    images_dir = '../../../images'
-    for filename in os.listdir('../../../images'):
+    images_dir = '../images'
+    for filename in os.listdir(images_dir):
         f = os.path.join(images_dir, filename)
-        img = cv2.imread(f, cv2.IMREAD_COLOR)
-        imgs.append(img)
+        cvimg = cv2.imread(f, cv2.IMREAD_COLOR)
+        rosimg = bridge.cv2_to_imgmsg(cvimg, encoding="passthrough")
+        imgs.append(rosimg)
 
     rospy.loginfo(imgs)
-    while not rospy.is_shutdown():
-        hello_str = "hello world %s" % rospy.get_time()
-        rospy.loginfo(hello_str)
-        pub.publish(hello_str)
-        rate.sleep()
+    
+    for img in imgs:
+        pub.publish(img)
+        
 
 if __name__ == '__main__':
     try:
