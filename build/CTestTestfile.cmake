cd8618e600c83e062e96a35a74e09a738ba4e61b
Ahmet Akif <aakifdemir07@gmail.com>
2022-10-26 20:18:57 +0300

    oldugu kadar hocam

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/aakif/python_catkin/src
+# Build directory: /home/aakif/python_catkin/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("mypackage")
---
+++ build/mypackage/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/aakif/python_catkin/src/mypackage
+# Build directory: /home/aakif/python_catkin/build/mypackage
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
